a731d334900d5393d78de5e18561265825492c0a
Kevin Wang <kevmo314@gmail.com>
2020-08-30 23:03:38 -0400

    Sticky header.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,12 +13,25 @@ const Header = styled.header`
   height: 840px;
 `;
 
-const Navigation = styled.nav`
-  display: flex;
+const Navigation = styled.nav<{ isJsEnabled: boolean; transparent: boolean }>`
+  background-color: ${({ transparent }) =>
+    transparent ? "transparent" : "white"};
+  box-shadow: ${({ transparent }) =>
+    transparent ? "none" : "1px 1px 8px #333"};
+  transition: all 0.3s;
+  position: ${({ isJsEnabled }) => (isJsEnabled ? "fixed" : "absolute")};
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
+
+const Gutters = styled.div`
+  margin: 0 auto;
   max-width: 1600px;
   width: 100%;
-  margin: 0 auto;
   padding: 16px 48px;
+  display: flex;
 `;
 
 const Links = styled.div`
@@ -46,24 +59,42 @@ const NavLink = styled(Link)`
 
 const Logo = styled.img`
   width: 150px;
+  margin-bottom: 0;
 `;
 
 const Content = styled.div`
   max-width: 1600px;
   margin: 0 auto;
+  margin-top: 120px;
 `;
 
-export default ({ children }: React.PropsWithChildren<{}>) => (
-  <Header>
-    <Navigation>
-      <Link to="/">
-        <Logo src={logo} />
-      </Link>
-      <Links>
-        <NavLink to="/join-us">join us</NavLink>
-        <NavLink to="/reasons-why">more reasons why</NavLink>
-      </Links>
-    </Navigation>
-    <Content>{children}</Content>
-  </Header>
-);
+export default ({ children }: React.PropsWithChildren<{}>) => {
+  const [top, setTop] = React.useState(true);
+  const [isJsEnabled, setJsEnabled] = React.useState(false);
+
+  React.useEffect(() => {
+    const listener = () => {
+      setTop(document.body.getBoundingClientRect().top === 0);
+    };
+    setJsEnabled(true);
+    window.addEventListener("scroll", listener);
+    return () => window.removeEventListener("scroll", listener);
+  }, []);
+
+  return (
+    <Header>
+      <Navigation transparent={top} isJsEnabled={isJsEnabled}>
+        <Gutters>
+          <Link to="/">
+            <Logo src={logo} />
+          </Link>
+          <Links>
+            <NavLink to="/join-us">join us</NavLink>
+            <NavLink to="/reasons-why">more reasons why</NavLink>
+          </Links>
+        </Gutters>
+      </Navigation>
+      <Content>{children}</Content>
+    </Header>
+  );
+};
